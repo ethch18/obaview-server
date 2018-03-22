6b85072f8e468104898584fd620f68bc7e82db8b
Ethan Chau <echau18@cs.uw.edu>
2018-03-21 21:30:13 -0700

    frontend api call working

--- proj/app/components/MainPage.js
+++ proj/app/components/MainPage.js
@@ -9,23 +9,25 @@ export default class MainPage extends React.Component {
             temp: null,
         };
         this.search = this.search.bind(this);
+        this.updateTemp = this.updateTemp.bind(this);
+    }
+
+    updateTemp(response) {
+        this.setState({ temp: JSON.stringify(response.data.data) });
+        console.log(response);
     }
 
     search(query) {
-        axios.get('http://localhost:8080/hello')
-            .then(function (response) {
-                this.setState({ temp: response });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        axios.get('http://localhost:8080/stops-for-route/1_100224')
+            .then(this.updateTemp)
+            .catch((error) => console.log(error));
     }
 
     render() {
         return (
         <div>
             <SearchBar searchFunction={this.search} />
-            <div>{!!this.state.temp}</div>
+            <div>{this.state.temp}</div>
         </div>
         );
     }
