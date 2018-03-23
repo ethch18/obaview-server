0dcc5a2b51a2d36a5718f84ecdd70d86fda17d06
Ethan Chau <echau18@cs.uw.edu>
2018-03-22 20:23:46 -0700

    is it just a naming thing?'

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
-from flask import Flask
-app = Flask(__name__)
+from bottle import Bottle, hook, request, response, route, run, template
+from constants import *
+import requests as rq
+from secret import KEY
+app = Bottle()
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+# hacky fix for port mangling, hopefully this won't be an issue later on
+@app.hook('after_request')
+def set_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS' 
+    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+@route('/', method = 'OPTIONS')
+@route('/<path>', method = 'OPTIONS')
+def options_handler(path = None):
+    return
+
+@route('/')
+@app.route('/hello')
+@app.route('/hello/<name>')
+def greet(name='Stranger'):
+    print("route")
+    print(request)
+    print(response)
+    return template('Hello {{name}}, how are you?', name=name)
+
+def make_request(url, request_id):
+    if not request_id:
+        return 'BUT IT FAILED!'
+    request_url = '{0}{1}.json?key={2}'.format(url, request_id, KEY)
+    print(request_url)
+    response = rq.get(request_url)
+    return response.json()
+
+@app.route('/stops-for-route/<route_id>')
+def stops_for_route(route_id):
+    return make_request(STOPS_FOR_ROUTE, route_id)
+
+@app.route('/stop/<stop_id>')
+def stop(stop_id):
+    return make_request(STOP, stop_id)
+
+@app.route('/arrivals-departures/<stop_id>')
+def arrivals_departures(stop_id):
+    return make_request(ARR_DEP, stop_id)
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
