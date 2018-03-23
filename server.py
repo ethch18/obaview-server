36dd0cbf0c0784a15d8480d7da0f8219216844c8
Ethan Chau <echau18@cs.uw.edu>
2018-03-22 21:20:48 -0700

    there's literally nothing here

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from bottle import Bottle, hook, request, response, route, run, template
-from constants import *
-import requests as rq
+# from constants import *
+# import requests as rq
 # from secret import KEY
 app = Bottle()
 
@@ -22,30 +22,27 @@ def options_handler(path = None):
 @app.route('/hello')
 @app.route('/hello/<name>')
 def greet(name='Stranger'):
-    print("route")
-    print(request)
-    print(response)
     return template('Hello {{name}}, how are you?', name=name)
 
-def make_request(url, request_id):
-    if not request_id:
-        return 'BUT IT FAILED!'
-    request_url = '{0}{1}.json?key={2}'.format(url, request_id, KEY)
-    print(request_url)
-    response = rq.get(request_url)
-    return response.json()
+# def make_request(url, request_id):
+#     if not request_id:
+#         return 'BUT IT FAILED!'
+#     request_url = '{0}{1}.json?key={2}'.format(url, request_id, KEY)
+#     print(request_url)
+#     response = rq.get(request_url)
+#     return response.json()
 
-@app.route('/stops-for-route/<route_id>')
-def stops_for_route(route_id):
-    return make_request(STOPS_FOR_ROUTE, route_id)
+# @app.route('/stops-for-route/<route_id>')
+# def stops_for_route(route_id):
+#     return make_request(STOPS_FOR_ROUTE, route_id)
 
-@app.route('/stop/<stop_id>')
-def stop(stop_id):
-    return make_request(STOP, stop_id)
+# @app.route('/stop/<stop_id>')
+# def stop(stop_id):
+#     return make_request(STOP, stop_id)
 
-@app.route('/arrivals-departures/<stop_id>')
-def arrivals_departures(stop_id):
-    return make_request(ARR_DEP, stop_id)
+# @app.route('/arrivals-departures/<stop_id>')
+# def arrivals_departures(stop_id):
+#     return make_request(ARR_DEP, stop_id)
 
 if __name__ == '__main__':
     app.run()
