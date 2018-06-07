25a51f0182d1f2018b29c5c9c08560134ecfd8e4
Ethan Chau <echau18@cs.uw.edu>
2018-06-06 22:16:07 -0700

    another partial uncomment

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ from secret import KEY
 
 app = Bottle()
 
+DEBUG = False
+
 # hacky fix for port mangling, hopefully this won't be an issue later on
 @app.hook('after_request')
 def set_cors():
-    # if DEBUG:
-    #     print('Running in DEBUG mode, all origins allowed')
-    #     response.headers['Access-Control-Allow-Origin'] = '*'
-    # else:
-    #     response.headers['Access-Control-Allow-Origin'] =  'https://echau18.gitlab.io'
+    if DEBUG:
+        print('Running in DEBUG mode, all origins allowed')
+        response.headers['Access-Control-Allow-Origin'] = '*'
+    else:
+        response.headers['Access-Control-Allow-Origin'] =  'https://echau18.gitlab.io'
     response.headers['Access-Control-Allow-Origin'] =  'https://echau18.gitlab.io'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS' 
     response.headers['Access-Control-Allow-Headers'] = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
